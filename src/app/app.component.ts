23db4f97380fb1106daed44761f7300c0339e037
Tiago.Antonio <tiago.antonio@mv.com.br>
2025-01-10 00:09:57 -0300

    [NVE000] Finalizando o projeto.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./pages/navbar/navbar.component";
+import { FooterComponent } from "./pages/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    RouterOutlet
+    RouterOutlet,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
