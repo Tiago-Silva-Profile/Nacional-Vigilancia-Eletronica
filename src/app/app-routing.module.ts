7e71cef6dc04b88d1532cae87061d04417be6c02
Tiago.Antonio <tiago.antonio@mv.com.br>
2025-01-13 11:18:34 -0300

    [NVE-0000] Atualizando o pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,6 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -6,12 +8,25 @@ import { ImageManagerComponent } from './pages/image-manager/image-manager.compo
 import { ServicesComponent } from './pages/services/services.component';
 import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [
+
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
   { path: 'image', component: ImageManagerComponent },
-  { path: 'service', component: ServicesComponent},
-  { path: 'login', component:LoginComponent},
-  { path: '**', redirectTo: '', pathMatch: 'full' } // Redireciona para Home caso a rota seja desconhecida
+  { path: 'service', component: ServicesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+})
+export class AppRoutingModule { }
