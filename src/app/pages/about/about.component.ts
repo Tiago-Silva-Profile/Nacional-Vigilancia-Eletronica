66266686139124d64f980e78e185da7f7b39216e
8okqb5s7 <tiagosilva.profile@gmail.com>
2025-01-19 00:20:44 -0300

    [NVE0000] Atualizando branch web.

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
