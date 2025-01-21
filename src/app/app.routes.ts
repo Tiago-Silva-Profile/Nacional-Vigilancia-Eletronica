f086d4bbf4150a6b2840fe2c6a02769a51b3075f
8okqb5s7 <tiagosilva.profile@gmail.com>
2025-01-21 13:22:00 -0300

    [NVE0000] Atualizando para o github pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ImageManagerComponent } from './pages/image-manager/image-manager.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
   { path: 'image', component: ImageManagerComponent },
@@ -15,3 +16,10 @@ export const routes: Routes = [
   { path: 'login', component:LoginComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' } // Redireciona para Home caso a rota seja desconhecida
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // Habilite o hash routing
+  exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
